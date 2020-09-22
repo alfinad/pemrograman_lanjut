6cbda2278eecef4237a3c01a416c021e1cbb1288
alfinad <alfinadmy1921@gmail.com>
2020-09-22 19:35:18 +0700

    first commit

---
+++ biodata saya.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	system("color B");
+	printf("========================.:: BIODATA ::.========================\n\n");
+	printf("Nama			: Alfina Damayanti\n");
+	printf("NPM			: 19081010039\n");
+	printf("Jurusan/Fakultas	: Informatika/Fakultas Ilmu Komputer\n");
+	printf("Tempat, Tanggal Lahir	: Nganjuk, 19 Februari 2001\n");
+	printf("Alamat			: Desa Perning, Kecamatan Jatikalen, Kabupaten Nganjuk\n");
+	printf("Agama			: Islam\n");
+	printf("Jenis Kelamin		: Perempuan\n");
+	printf("Kewarganegaraan		: WNI\n");
+	printf("Pekerjaan		: Pelajar/Mahasiswa\n");
+	printf("Nomor HP		: 085736991010\n");
+	printf("Email			: alfinad1921@gmail.com\n");
+	
+	return 0;
+}
